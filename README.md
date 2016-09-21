c3f481ee5c9c25a152ccc6f935e8eecc3564172e
suresh kumar <sureshkumar.s@altencalsoftlabs.com>
2016-09-21 14:27:33 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-plugin
+
+http://www.giantflyingsaucer.com/blog/?p=5858
---
+++ myplugin/drivers/mydriver.py
@@ -0,0 +1,9 @@
+class Driver1(object):
+    def getname(self):
+        print("getname called from Driver1")
+        return
+
+
+class Driver2(object):
+    def getname(self):
+        print("getname called from Driver2")
